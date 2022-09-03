b0b10d042daa337827802aafe0d7e865d11219ed
Vitor Augusto <vitorpfiffer@gmail.com>
2022-09-03 18:53:58 -0300

    Implementacao com Axios
    axios ja consegue acessar a api externa e  receber o token
    -> Falta descobrir como manipular essa resposta para poder enviar um header com o token em um get ou post

--- front/src/App.js
+++ front/src/App.js
@@ -2,43 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://localhost:7008/'
-//axios.defaults.headers.common = {'Authorization': `bearer ${token}`}
-var Token
 
-function GetPictures(){
-  axios.post({
-    baseURL: 'https://localhost:7008/users/authentication',
-    headers:{},
-    data:{
+const url = 'https://localhost:7008/users/authenticate'
+var a 
+function Authenticate()
+{
+    axios.post(url,{
       username:"test",
       password:"test"
-    }
-  }).then(response => console.log(response)).catch(error => console.log(error))
-
-
+    }).then(response => {
+      const data = response.data
+      a = JSON.stringify(data) 
+    }).catch(error => console.log(error))
 }
 
-
+Authenticate()
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{a}</p>
+      <p>vasco</p>
     </div>
   );
 }
-GetPictures()
+Authenticate()
 export default App;
