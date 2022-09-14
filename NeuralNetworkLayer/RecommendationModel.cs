90f32420f7d5eb8a2efecd94327d2f130cdea0f0
fcalderon29 <108772069+fcalderon29@users.noreply.github.com>
2022-09-14 16:46:16 -0300

    criando classe de categorização de perfil

---
+++ NeuralNetworkLayer/RecommendationModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace NeuralNetworkLayer
+{
+    internal class RecommendationModel
+    {
+        //Tag will be an externalclass by itself -- to be implemented
+        public static string CategorizeProfileByTags(List<string> tags)
+        {
+            InstagramProfile profile = new();
+
+            int sportBias = 0;
+            int exotericBias = 0;
+            int animeBias = 0;
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+
+            }
+
+
+            return "";
+        }
+
+
+    }
+}
